a6b042612e751445537c66e6cdc4c4825b5cfa17
23000018249 <2300018249@webmail.uad.ac.id>
2024-10-20 19:25:21 +0700

    Update App.java

--- App.java
+++ App.java
@@ -4,13 +4,14 @@ public class App {
     @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
         Cashier cashier = new Cashier();
-        //variable "cashier" --> instance of class Cashier 
+        //instance of class Cashier
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("1. Tambah Barang");
             System.out.println("2. Hapus Barang");
-            System.out.println("3. Tampilkan Total");
-            System.out.println("4. Keluar");
+            System.out.println("3. Tampilkan Item dan Subtotal");
+            System.out.println("4. Tampilkan Total dan Proses Pembayaran");
+            System.out.println("5. Keluar");
             System.out.print("Pilih opsi: ");
             int choice = scanner.nextInt();
             switch (choice) {
@@ -21,7 +22,9 @@ public class App {
                     String addName = scanner.next();
                     System.out.print("Masukkan harga barang: ");
                     double addPrice = scanner.nextDouble();
-                    cashier.addItem(addPrice, addCode, addName);
+                    System.out.print("Masukkan jumlah barang: ");
+                    int addQuantity = scanner.nextInt();
+                    cashier.addItem(addPrice, addCode, addName, addQuantity);
                     break;
                 case 2:
                     System.out.print("Masukkan kode barang yang akan dihapus: ");
@@ -29,9 +32,15 @@ public class App {
                     cashier.removeItem(removeCode);
                     break;
                 case 3:
-                    cashier.displayTotal();
+                    cashier.displayItems();
                     break;
                 case 4:
+                    cashier.displayTotal();
+                    System.out.print("Masukkan jumlah uang yang dibayarkan: ");
+                    double payment = scanner.nextDouble();
+                    cashier.processPayment(payment);
+                    break;
+                case 5:
                     System.out.println("Terima kasih telah menggunakan sistem kasir kami!");
                     return;
                 default:
